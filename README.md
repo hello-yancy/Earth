bf9d844f9a665b3a3d051915f287bba648db34e8
hello-yancy <yujd2000@sina.com>
2019-08-03 01:16:59 +0800

    add googletest

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # earth
+
+# 一、 三方件说明
+## googletest
+下载地址：https://github.com/google/googletest  
+版本名称：1.8.1  
+
+## glog
+下载地址：https://github.com/google/glog  
+版本名称：0.4.0    
+
---
+++ src/3rdTools/googletest/build.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+SCRIPT_NAME=`basename ${BASH_SOURCE}`
+
+PROJECT_ROOT_PATH=$(dirname $(dirname $(dirname $(pwd))))
+INCLUDE_PATH=${PROJECT_ROOT_PATH}/include
+LIB_PATH=${PROJECT_ROOT_PATH}/lib
+
+SOURCE_CODE_PATH=${PROJECT_ROOT_PATH}/src/3rdTools/googletest
+PACKED_SOURCE_CODE=googletest-release-1.8.1.zip
+UNPACKED_SOURCE_CODE=googletest-release-1.8.1
+PACKED_SOURCE_CODE_PATH=${SOURCE_CODE_PATH}/${PACKED_SOURCE_CODE}
+UNPACKED_SOURCE_CODE_PATH=${SOURCE_CODE_PATH}/${UNPACKED_SOURCE_CODE}
+
+LIB_OUT_PATHS=(
+    build/googlemock/libgmock.a
+    build/googlemock/libgmock_main.a
+    build/googlemock/gtest/libgtest.a
+    build/googlemock/gtest/libgtest_main.a
+)
+
+HEADER_FILE_PATHS=(
+    googlemock/include/gmock
+    googletest/include/gtest
+)
+
+log_info() {
+    echo "[${SCRIPT_NAME}]@`date "+%Y-%m-%d %H:%M:%S"`[INFO]: $* "
+}
+
+log_error() {
+    echo "[${SCRIPT_NAME}]@`date "+%Y-%m-%d %H:%M:%S"`[ERROR]: $* "
+}
+
+print_parameters() {
+    log_info "SCRIPT_NAME = $SCRIPT_NAME"
+    log_info "PROJECT_ROOT_PATH = $PROJECT_ROOT_PATH"
+    log_info "INCLUDE_PATH = $INCLUDE_PATH"
+    log_info "LIB_PATH = $LIB_PATH"
+    log_info "SOURCE_CODE_PATH = $SOURCE_CODE_PATH"
+    log_info "PACKED_SOURCE_CODE = $PACKED_SOURCE_CODE"
+    log_info "UNPACKED_SOURCE_CODE = $UNPACKED_SOURCE_CODE"
+    log_info "PACKED_SOURCE_CODE_PATH = $PACKED_SOURCE_CODE_PATH"
+    log_info "UNPACKED_SOURCE_CODE_PATH = $UNPACKED_SOURCE_CODE_PATH"
+}
+
+pre_build() {
+    log_info "pre_build enter"
+
+    if [ -d ${UNPACKED_SOURCE_CODE_PATH} ]; then
+        log_info "${UNPACKED_SOURCE_CODE_PATH} already exist, will exit."
+        exit 0
+    fi
+
+    if [ ! -f ${PACKED_SOURCE_CODE_PATH} ]; then
+        log_error "${UNPACKED_SOURCE_CODE_PATH} does not exist, will exit."
+        exit 0
+    fi
+
+    log_info "starting unzip file"
+    unzip ${PACKED_SOURCE_CODE_PATH} > /dev/null
+
+    log_info "pre_build exit"
+}
+
+build() {
+    log_info "build enter"
+
+    if [ ! -d ${UNPACKED_SOURCE_CODE_PATH} ]; then
+        log_error "${UNPACKED_SOURCE_CODE_PATH} does not exist, will exit."
+        exit 0
+    fi
+
+    cd ${UNPACKED_SOURCE_CODE_PATH}
+    mkdir ./build
+    cd ./build
+    cmake ../
+    make
+
+    log_info "build exit"
+}
+
+post_build() {
+    log_info "post_build enter"
+
+    move_files_to_include
+    move_libs_to_lib
+
+    log_info "post_build exit"
+}
+
+move_files_to_include() {
+    log_info "starting move_files_to_include"
+
+    mkdir -p ${INCLUDE_PATH}
+    for file_path in ${HEADER_FILE_PATHS[@]}; do
+        cp -R ${UNPACKED_SOURCE_CODE_PATH}/${file_path} ${INCLUDE_PATH}
+    done
+}
+
+move_libs_to_lib() {
+    log_info "starting move_libs_to_lib"
+
+    mkdir -p ${LIB_PATH}
+    for lib_path in ${LIB_OUT_PATHS[@]}; do
+        cp ${UNPACKED_SOURCE_CODE_PATH}/${lib_path} ${LIB_PATH}
+    done
+}
+
+main() {
+    print_parameters
+    pre_build
+    build
+    post_build
+}
+
+main
